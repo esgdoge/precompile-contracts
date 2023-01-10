39c7cabea073494b29b5702a0f81c7804850ac81
tyler-doge <tyler@jazdid.com>
2023-01-10 15:21:12 +0800

    update

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,9 @@
 const { ethers } = require('hardhat')
 const localContracts = require('./contracts')
 
+// const NewCollatorPrivateKey = "10771b1c06e52bec6cca5fc8d6d144526edcafc39a89e14b8a1a19166c98b306"
+// const NewCollatorAddress = '0x2DB8E5B7FAf3680dC6f83e576e2caF5054bca600'
+
 async function transferOwnership() {
   let contracts = await localContracts.getContracts()
 
@@ -14,11 +17,11 @@ async function transferOwnership() {
   await tx.wait()
 }
 
-async function mint(contracts) {
+async function mint(contracts, to, role) {
   // let tx = await contracts.nodeManager.setCurTokenId(1)
   // console.log(`NodeNFT setCurTokenId: ${tx.hash}`)
 
-  let tx = await contracts.nodeManager.mint(contracts.deployer.address, Buffer.from('SuperRare'))
+  let tx = await contracts.nodeManager.mint(to, Buffer.from(role))
   console.log(`NodeNFT mint: ${tx.hash}`)
   await tx.wait()
 }
@@ -48,21 +51,48 @@ async function unbond(contracts, tokenId, value) {
   console.log(`NodeStaking unbond: ${tx.hash}`)
 }
 
-async function main() {
-  let contracts = await localContracts.getContracts()
-  // await transferOwnership()
+async function setSessionKey(contracts, key) {
+  let tx = await contracts.nodeSession.setKeys(key)
+  console.log(`NodeSession setKeys: ${tx.hash}`)
+}
 
-  await mint(contracts)
+// must new wallet call
+async function collatorActions(to) {
+  let contracts = await localContracts.getContracts()
+
+  await setSessionKey(contracts, '0x5e0de8b3ecbf1d142dbd709c3b9c0d2cbf137258e206305f3dd84c0046c78d7' +
+    '05b5cb4e51602558225dc4ca905d0d848be58f3ddf02ada94f56ace812a1feaf108bcf30f383bdcb21cf58693543d190363d' +
+    '8d5a513d98f877698e419b8f7121cca7a89829d809c2f5589b8ae999d578d5fbc43106ae4309833068230202c5a35')
 
   const tokenId = await contracts.nodeManager.curTokenId()
-  await bind(contracts, tokenId)
-  await delegate(contracts, contracts.deployer.address, tokenId)
+  await bind(contracts, to, tokenId)
 
   await bond(contracts, tokenId, ethers.utils.parseEther('50'))
+}
 
+async function delegateActions() {
+  let contracts = await localContracts.getContracts()
+
+  await mint(contracts, 'Rare')
+
+  const tokenId = await contracts.nodeManager.curTokenId()
+  await delegate(contracts, contracts.deployer.address, tokenId)
+
+  await bond(contracts, tokenId, ethers.utils.parseEther('30'))
+}
+
+async function unbondActions() {
+  let contracts = await localContracts.getContracts()
+  const tokenId = await contracts.nodeManager.curTokenId()
   await unbond(contracts, tokenId, ethers.utils.parseEther('10'))
-
   await unbind(contracts, tokenId)
+}
+
+async function main() {
+  let contracts = await localContracts.getContracts()
+  // await mint(contracts, contracts.deployer.address, 'SuperRare')
+
+  await delegateActions(contracts.deployer.address)
 }
 
 main()
